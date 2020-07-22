80c2bb380b5972484058ce919cc6e63f780d036e
upepp <gfragoso83@gmail.com>
2020-07-22 16:11:24 -0300

    segundo

---
+++ database/migrations/2020_07_22_170711_create_dormrooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDormroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dormrooms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->float('price');
+            $table->string("adress");
+            $table->integer('free_dorms') -> nullable();
+            $table->string('name_of_the_property') -> unique();
+            $table->string('adicional_prices')-> nullable();
+            $table->string('phone');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dormrooms');
+    }
+}
